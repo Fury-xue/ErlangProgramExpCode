4e322ed38de0abfed696c2654d1a4ca72f83d59a
fury-xue <shu0516@qq.com>
2015-10-22 10:22:38 +0800

    add my_date_string function

--- chapter4/practice/practice.erl
+++ chapter4/practice/practice.erl
@@ -14,19 +14,31 @@
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
-
+-spec my_tuple_to_list(Arg :: tuple()) -> List :: list();
+                      (Arg :: term()) -> ok.
 my_tuple_to_list(Arg) when is_tuple(Arg) ->
     Parse = erl_parse:abstract(Arg),
-    io:format("~p~n",[add_to_list(Parse)]);
+    List = add_to_list(Parse),
+    io:format("~p~n",[List]),
+    List;
 my_tuple_to_list(Arg) when not is_tuple(Arg) ->
     io:format("the arg is not a tuple~n").
+
+-spec my_time_fun(F :: function()) -> ok.
 
 my_time_fun(F) when is_function(F,0) ->
     {H,M,S} = erlang:time(),
     F(),
     {AH,AM,AS} = erlang:time(),
-    io:format("the function start time : ~pH~pM~pS, and stop time : ~pH~pM~pS~n",[H,M,S,AH,AM,AS]),
-    io:format("diffent time : ~p~n",[Time]).
+    io:format("the function start time : ~pH~pM~pS, and stop time : ~pH~pM~pS~n",[H,M,S,AH,AM,AS]).
+
+-spec my_date_string() -> Now :: atom().
+my_date_string() ->
+    {Y,M,D} = erlang:date(),
+    {AH,AM,AS} = erlang:time(),
+    Now = erlang:integer_to_list(Y) ++ "/" ++ string:right(erlang:integer_to_list(M),2,$0) ++ "/" ++ string:right(erlang:integer_to_list(D),2,$0) ++ " " ++ string:right(erlang:integer_to_list(AH),2,$0) ++ ":" ++ string:right(erlang:integer_to_list(AM),2,$0) ++ " " ++ string:right(erlang:integer_to_list(AS),2,$0),
+    list_to_atom(Now).
+    
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
